8b2e5af9e9c4db3b03e9f3a7faaf83284ef9a372
DJ-MESH <koechmeshack77@gmail.com>
2025-01-06 17:06:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_16_64_59',
 	SUDO: process.env.SUDO || '',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
 	BOT_INFO: process.env.BOT_INFO || 'Mesh_Tech',
-	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
+	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;Mesh KE мυℓтι ∂єνι¢є вσт',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
 	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Nairobi',
 	DEBUG: process.env.DEBUG || false,
